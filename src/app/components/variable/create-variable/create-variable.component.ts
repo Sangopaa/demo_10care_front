94e5858735401c7a4b4a70b8bc0fce5a7be856d1
Sangopaa <sangopaa@gmail.com>
2023-11-13 23:43:09 -0500

    avances en el front

--- src/app/components/variable/create-variable/create-variable.component.ts
+++ src/app/components/variable/create-variable/create-variable.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { VariableService } from '../services/variable.service';
+import { typeVariables } from '../interfaces/type_variables';
 
 @Component({
   selector: 'app-create-variable',
@@ -10,16 +11,15 @@ import { VariableService } from '../services/variable.service';
 export class CreateVariableComponent {
   createVariableForm: FormGroup;
 
-  opcionesSelect = [
-    { value: '', label: '' },
-  ];
+  opcionesSelect:typeVariables[] = [];
+
 
   constructor(
     private formBuilder: FormBuilder,
     private service: VariableService
   ) {
     this.createVariableForm = this.formBuilder.group({
-      data_variable: ['', Validators.required],
+      value_data_type: ['', Validators.required],
       name_variable: ['', Validators.required],
     });
   }
@@ -30,7 +30,7 @@ export class CreateVariableComponent {
         // Maneja la respuesta exitosa
 
         this.opcionesSelect = response.type_variable.map((item: any) => ({
-          value: item.type_variable.toString(),
+          value: Number(item.type_variable),
           label: item.name_tipe_variable,
         }));
       },
@@ -44,7 +44,11 @@ export class CreateVariableComponent {
   submitForm() {
     if (this.createVariableForm.valid) {
       // Realiza acciones con los datos del formulario
-      console.log('Formulario válido:', this.createVariableForm.value);
+      const valueDataType = Number(this.createVariableForm.get('value_data_type')?.value);
+
+      if(valueDataType === 5){
+        console.log('holaa');
+      }
     } else {
       // Muestra mensajes de error o realiza acciones necesarias para formularios no válidos
     }
---
+++ src/app/components/variable/interfaces/type_variables.ts
@@ -0,0 +1,4 @@
+export interface typeVariables {
+  value: number;
+  label: string;
+}
